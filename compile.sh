6f761d6d0d5143f5977c0fda2c965c5bfaf5b513
Israel Medina <isra2dave@gmail.com>
2024-02-02 17:40:49 -0300

    chore(build): update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-/usr/bin/glslc src/shaders/simple_shader.vert -o src/shaders/simple_shader.vert.spv
-/usr/bin/glslc src/shaders/simple_shader.frag -o src/shaders/simple_shader.frag.spv
+/usr/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/usr/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+/usr/bin/glslc shaders/point_light.vert -o shaders/point_light.vert.spv
+/usr/bin/glslc shaders/point_light.frag -o shaders/point_light.frag.spv
