b27b5d679852b54b366f07e1d2de7123a0cacbed
Israel Medina <isra2dave@gmail.com>
2024-02-03 00:50:57 -0300

    chore(build): update build script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
-/usr/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-/usr/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
-/usr/bin/glslc shaders/point_light.vert -o shaders/point_light.vert.spv
-/usr/bin/glslc shaders/point_light.frag -o shaders/point_light.frag.spv
+#!/bin/bash
+mkdir -p build
+cd build
+cmake -S ../ -B .
+make && make Shaders && ./DvmEngine
+cd ..
