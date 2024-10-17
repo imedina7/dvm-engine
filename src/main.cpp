205536d2513ea2bb4a39e8b67a6d2be9bee6829c
Israel Medina <contact@israelmedina.com>
2024-10-17 17:32:19 -0300

    feat(vulkan): added dynamic library loading code, added c++ flags

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,48 @@
 #include <cstdlib>
 #include <stdexcept>
 
+#ifdef MACOS
+#ifdef VULKAN_SDK
+  #define MACOS_WITH_VULKAN
+#endif
+#endif
+
+#ifdef MACOS_WITH_VULKAN
+  void* load_vulkan_dynlib() {
+    std::cout << "VULKAN_SDK path: " << VULKAN_SDK << "\n";
+
+    void* handle = dlopen(VULKAN_SDK + "/libvulkan.1.dylib", RTLD_LAZY);
+    if (!handle) {
+        std::cerr << "Error: " << dlerror() << std::endl;
+        return nullptr;
+    }
+
+    return handle;
+  }
+#endif
+
+
 int main()
 {
+  #ifdef MACOS_WITH_VULKAN
+    void* vk_load_handle = load_vulkan_dynlib();
+    if(vk_load_handle === nullptr) return EXIT_FAILURE;
+  #endif
+
   dvm::DvmApp& app = dvm::DvmApp::getInstance();
   try {
     app.run();
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
+
+    #ifdef MACOS_WITH_VULKAN
+      dlclose(vk_load_handle);
+    #endif
     return EXIT_FAILURE;
   }
+
+  #ifdef MACOS_WITH_VULKAN
+    dlclose(vk_load_handle);
+  #endif
   return EXIT_SUCCESS;
 }
