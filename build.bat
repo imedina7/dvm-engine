b510c061d17f4387fd8560de42bf61cb6fe39b39
Israel Medina <contact@israelmedina.com>
2025-08-13 21:48:20 -0300

    chore(config): environment configuration

--- build.bat
+++ build.bat
@@ -4,10 +4,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliar
 
 SET GLFW_PATH="C:\VulkanSDK\Libraries\glfw-3.3.9.bin.WIN64"
 
-SET includes=/Isrc /I%VULKAN_SDK%/Include /Isrc /I%GLFW_PATH%/include
-SET links=/link /LIBPATH:%VULKAN_SDK%/Lib /LIBPATH:%GLFW_PATH%/lib-vc2022 vulkan-1.lib glfw3_mt.lib
-SET defines=/D DEBUG
-
-echo "Building main..."
-
-cl/EHsc %includes% %defines% src/main.cpp %links%
+cmake --build build
