f1324e7489301477606b82e72b807522bd43783a
Israel Medina <contact@israelmedina.com>
2025-08-18 09:09:55 -0300

    chore(win): update build script

--- build.bat
+++ build.bat
@@ -3,5 +3,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 SET GLFW_PATH="C:\VulkanSDK\Libraries\glfw-3.3.9.bin.WIN64"
-
+mkdir build
+cmake -S . -B build
 cmake --build build
